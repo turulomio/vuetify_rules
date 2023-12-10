5b911226732adad2139d16e07eb42d90d06bb7f2
turulomio <turulomio@yahoo.es>
2023-12-10 11:37:01 +0100

    Works with mocha

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 import { isNullOrEmpty } from "./src/functions.js";
 
-console.log(isNullOrEmpty("HOLA"))
-
 export const vuetify_rules={
     isNullOrEmpty
-
 }
 
--- test/functions.test.js
+++ test/functions.test.js
@@ -1,8 +1,8 @@
-var vuetify_rules=require("isnullorempty")
-describe("the add function", () => {
-    it("should add two numbers", () => {
-      const result = vuetify_rules.isNullOrEmpty( 2);
-      expect(vuetify_rules.isNullOrEmpty( 2)).toBe(false)
+import { vuetify_rules } from '../index.js';
+import assert from "assert"
 
-    });
+describe("Functions", () => {
+  it('isNullOrEmpty', () => {
+    assert.equal(vuetify_rules.isNullOrEmpty( 5), false);
   });
+});
