d0adbcb17fcfced025230b9e6c9716c6f58d4f65
turulomio <turulomio@yahoo.es>
2023-12-10 10:08:43 +0100

    Works

--- test/index.test.js
+++
@@ -1,8 +0,0 @@
-var vuetify_rules=require("../index.js")
-describe("the add function", () => {
-    it("should add two numbers", () => {
-      const result = vuetify_rules.isNullOrEmpty( 2);
-      expect(vuetify_rules.isNullOrEmpty( 2)).toBe(false)
-
-    });
-  });
