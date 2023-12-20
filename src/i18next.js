449c86bfae70bdb1b5c973888dffe9aeb7f83c31
turulomio <turulomio@yahoo.es>
2023-12-20 19:42:35 +0100

    Removed await from root (#13)

--- src/i18next.js
+++ src/i18next.js
@@ -3,29 +3,34 @@ import Backend from 'i18next-fs-backend';
 
 
 
-i18next
-    .use(Backend)
-    .init({
-        initImmediate: false,
-        debug: false,  
-        lng: [
-            'en',
-            'es',
-            'fr',
-         ],
-        backend: {
-            loadPath: './locales/{{lng}}/{{ns}}.json',
-            // path to post missing resources
-            addPath: './locales/{{lng}}/{{ns}}.missing.json',
-        },  
-        fallbackLng: "en"
-    });
-var locale=Intl.DateTimeFormat().resolvedOptions().locale.slice(0,2)
-console.log("VuetifyRules detected language with Intl: ", locale )
-await change_language(locale)
-export const module_i18next=i18next
+export function vuetify_rules_i18next_init(){
+    i18next
+            .use(Backend)
+            .init({
+                initImmediate: false,
+                debug: false,  
+                lng: [
+                    'en',
+                    'es',
+                    'fr',
+                ],
+                backend: {
+                    loadPath: './locales/{{lng}}/{{ns}}.json',
+                    // path to post missing resources
+                    addPath: './locales/{{lng}}/{{ns}}.missing.json',
+                },  
+                fallbackLng: "en"
+            });
+    var locale=Intl.DateTimeFormat().resolvedOptions().locale.slice(0,2)
+    console.log("VuetifyRules detected language with Intl: ", locale )
+    vuetify_rules_i18next_change_language(locale)
+    return i18next
+}
 
-export async function change_language( locale) {
+export const vuetify_rules_i18next=vuetify_rules_i18next_init()
+
+
+export async function vuetify_rules_i18next_change_language( locale) {
     await i18next.changeLanguage(locale)
 
 }
